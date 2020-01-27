725efa0b3fa21e58479a005807cb26dc12634319
dumpstarfire <60335454+dumpstarfire@users.noreply.github.com>
2020-01-26 22:58:10 -0600

    Add files via upload

---
+++ sftp.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+command -v yum > /dev/null && OS=rhel
+command -v apt > /dev/null && OS=debian
+if [ "$OS" == "rhel" ] ; then
+     yum -y update
+     yum -y install vsftpd
+     yum -y install openssh-server
+else
+     apt-get -y update
+     apt-get -y install vsftpd
+     apt-get -y openssh-server
+fi
+sed 's/anonymous_enable=YES/anonymous_enable=YES/g' /etc/vsftpd/vsftpd.conf
+echo "chroot_local_user=YES" >> /etc/vsftpd/vsftpd.conf
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=Yes" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=40000" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=40100" >> /etc/vsftpd/vsftpd.conf
+
+groupadd sftp_users
+
+if [ "$OS" == "rhel" ] ; then
+     adduser $1
+     echo $2 | passwd $1 --stdin
+
+else
+     adduser --disabled-password --gecos "" $1
+     echo $1:$2 | chpasswd
+
+fi
+
+mkdir -p /data
+chmod 701 /data
+
+mkdir -p /data/$1/upload
+chown -R root:sftp_users /data/$1
+chown -R $1:sftp_users /data/$1/upload
+
+echo "Match Group sftp_users" >> /etc/ssh/sshd_config
+echo "ChrootDirectory /data/%u" >> /etc/ssh/sshd_config
+echo "ForceCommand internal-sftp" >> /etc/ssh/sshd_config
+#echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+sed -i 's/PasswordAuthentication\s*no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+read -p  "Do you want to allow root to login? y/n: " root_answer
+if [ "$root_answer" == "y" ] ; then
+    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    read -p "Setting root password: " root_password
+    if [ "$OS" == "rhel" ] ; then
+         echo $root_password | passwd root --stdin
+    else
+         echo root:$root_password | chpasswd
+    fi
+else
+    echo "Password Not Chosen"
+fi
+systemctl restart sshd
+systemctl restart vsftpd
